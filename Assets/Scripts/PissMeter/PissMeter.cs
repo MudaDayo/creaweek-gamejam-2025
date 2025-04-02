3c2aec3c4ada1b7c7900a1cf7ef2b3a47eba5cb7
Milena Smyk <milena.smyk.be@gmail.com>
2025-04-02 13:58:46 +0200

    PissMeterClass + Slider

---
+++ Assets/Scripts/PissMeter/PissMeter.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
+
+public class PissMeter : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    private int PissAmount = 0;
+}
