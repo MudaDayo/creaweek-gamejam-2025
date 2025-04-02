8614c1b6e1fc2927dd718b8980b65515f784ed74
Watse D'haene <watse.dhaene@gmail.com>
2025-04-02 21:12:44 +0200

    bark script created

---
+++ Assets/Audio/BarkScript.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class BarkScript : MonoBehaviour
+{
+    private AudioManager audioManager;
+
+    public int playerID = 1;
+
+    void Start()
+    {
+        audioManager = FindFirstObjectByType<AudioManager>();
+        audioManager?.PlaySound("LevelMusic");
+    }
+    void Update()
+    {
+        if (playerID == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                audioManager?.PlaySound("Bark1");
+            }
+        }
+
+        else
+        {
+            // east gamepad button pressed
+            if (Input.GetKeyDown("Crouch"))
+            {
+                audioManager?.PlaySound("Bark" + playerID);
+            }
+
+        }
+    }
+}
